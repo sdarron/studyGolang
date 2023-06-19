f394f0ef8c5f216f3dac18fb30966e853ec802c6
sdarron <zaytsev@bk.ru>
2023-06-19 22:31:09 +0300

    hw

--- go_for_beginer/homeWork01/main.go
+++ go_for_beginer/homeWork01/main.go
@@ -1,5 +1,35 @@
-// Перепишите программу для подсчета площади круга, используя число Пи из
-// библиотеки math . 
+// Перепишите программу для подсчета площади круга, 
+// используя число Пи из библиотеки math. 
 // Напишите функции для подсчета площади и периметра
 // прямоугольника и треугольника. 
-//
+
+package main
+
+import ( 
+	"fmt"
+	"errors"
+	"math"
+)
+func main()  {
+	printCircleArea(2)
+	printCircleArea(-2)
+}
+
+
+func printCircleArea( radius int ){
+	circleArea, err := calculateCircleArea( radius )
+	if err != nil{
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Printf("Радиус круга %d см.\n", radius)	
+	print("Формула для расчета круга: A=Pi*(r)2\n")
+	fmt.Printf("Площать круга: %f32 см. кв. \n", circleArea)
+}
+
+func calculateCircleArea(radius int) (float32, error) {
+	if radius <= 0 {
+		return float32(0), errors.New("Радиус круга не может быть отрицательным")
+	}
+	return float32(radius) * float32(radius) * math.Pi, nil
+}
