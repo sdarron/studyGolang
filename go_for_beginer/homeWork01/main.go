711dd6d43cf484c645d14080cf5f9668379e7ebc
sdarron <zaytsev@bk.ru>
2023-06-20 23:48:57 +0300

    Rest

--- go_for_beginer/homeWork01/main.go
+++ go_for_beginer/homeWork01/main.go
@@ -13,8 +13,29 @@ import (
 func main()  {
 	printCircleArea(2)
 	printCircleArea(-2)
+	printRectangleArea( 2, 4 )
+	// printTriangleArea()
+
 }
 
+
+func printRectangleArea( side1 int, side2 int ){
+	rectangleArea, err := calculateRectangleArea( side1, side2)
+	if err != nil{
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Printf("Стороны проямоугольника равны %d и %d см.\n", side1, side2)	
+	print("Формула для площади прямоугольника: S=a*b\n")
+	fmt.Printf("Площать прямоугольника: %d см. кв. \n", rectangleArea)
+}
+
+func calculateRectangleArea( side1 int, side2 int ) ( int, error ){
+	if side1 <=0 || side2 <= 0{
+		return 0, errors.New("Сторона прямоугольнка не может быть отрицательной")
+	}
+	return side1 * side2, nil
+}
 
 func printCircleArea( radius int ){
 	circleArea, err := calculateCircleArea( radius )
